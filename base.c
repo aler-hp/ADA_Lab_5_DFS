e5e8e8686e75a0b78c78915ae03f36669539eee0
aler-hp <autademontag@gmail.com>
2025-10-28 07:30:03 -0500

    base implementation from lab 4

---
+++ base.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define vertex int
+
+typedef struct graph *Graph;
+
+typedef struct node *link;
+
+struct graph {
+    int V;
+    int A;
+    link *adj;
+};
+
+struct node {
+    vertex w;  //value
+    link next;
+};
+
+static link NEWnode( vertex w, link next) {
+    link a = (link) malloc( sizeof (struct node));
+    a->w = w;
+    a->next = next;
+    return a;
+}
+
+Graph GRAPHinit( int V) {
+    Graph G = (Graph) malloc( sizeof *G);
+    G->V = V;
+    G->A = 0;
+    G->adj = (link *) malloc( V * sizeof (link));
+    for (vertex v = 0; v < V; ++v)
+        G->adj[v] = NULL;
+    return G;
+}
+
+void GRAPHinsertArc( Graph G, vertex v, vertex w) {
+    for (link a = G->adj[v]; a != NULL; a = a->next)
+        if (a->w == w) return;
+        G->adj[v] = NEWnode( w, G->adj[v]);
+        G->A++;
+}
+
+void GRAPHshow(Graph G) {
+    for (vertex v = 0; v < G->V; v++) {
+        printf("Vertice %d :", v);
+        for (link a = G->adj[v]; a != NULL; a = a->next)
+            printf("%d--", a->w);
+        printf("\n");
+    }
+}
+
+void GRAPHdestroy(Graph G) {
+    if (G == NULL) return;
+    for (vertex v = 0; v < G->V; v++) {
+        link a = G->adj[v];
+        while (a != NULL) {
+            link next = a->next;
+            free(a);
+            a = next;
+        }
+    }
+
+
+    free(G->adj);
+    free(G);
+}
+
+
+static int cnt;
+int pre[1000];
+
+void GRAPHdfs( Graph G)
+{
+cnt = 0;
+for (vertex v = 0; v < G->V; ++v)
+pre[v] = -1;
+for (vertex v = 0; v < G->V; ++v)
+if (pre[v] == -1)
+dfsR( G, v); // comienza nueva etapa
+
+}
+
+static void dfsR( Graph G, vertex v)
+{
+pre[v] = cnt++;
+for (link a = G->adj[v]; a != NULL; a = a->next) {
+vertex w = a->w;
+if (pre[w] == -1)
+dfsR( G, w);
+}
+}
